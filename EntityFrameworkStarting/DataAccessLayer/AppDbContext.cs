d97f37fbcaa9a15ef69c62a74360b13ca74a2226
ilqar01 <168280994+ilqar01@users.noreply.github.com>
2025-02-23 02:05:10 -0800

    EFcoreStarting01

---
+++ EntityFrameworkStarting/DataAccessLayer/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace EntityFrameworkStarting.DataAccessLayer
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = DESKTOP - COFH27A; Initial Catalog = UdemyEFCoreDatabaseFirstDb; Integrated Security = True;" +
+                " Connect Timeout = 30; Encrypt = False; TrustServerCertificate = False; " +
+                "ApplicationIntent = ReadWrite; MultiSubnetFailover = False");
+        }
+
+    }
+}
---
+++ EntityFrameworkStarting/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using EntityFrameworkStarting.DataAccessLayer;
+using (var _context = new AppDbContext())
+{
+	var products = _context.Products.ToList();
+
+
+    foreach (var item in products)
+    {
+        Console.WriteLine($"{item.Id}    {item.Name}    {item.Price}");
+    }
+
+}
+
+
+
